15e450a9a301996d92babd870b02c5c54f2ee841
Oleksandr Omelyan <jemm.ua@gmail.com>
2022-09-26 22:20:54 +0300

    Homeworks. Add Develop branch

--- src/HomeWorks/lesson09/mainStrings.java
+++ src/HomeWorks/lesson09/mainStrings.java
@@ -2,8 +2,8 @@ package HomeWorks.lesson09;
 
 public class mainStrings {
     public static void main(String[] args) {
-        String t = "sasdf e  dse";
-        System.out.println(t.contains("f "));
+
+        //Homework
 
     }
 }
